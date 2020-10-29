f44b3edcbc4d67bfe13442c3cf8bfed7be63e0ca
Bruno Theodoro <theodorodeaquino@gmail.com>
2020-10-29 07:08:37 -0300

    :package:JavaScript do Orçamento

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 var burger = document.querySelector(".burger");
 
-burger.addEventListener("click", function(){
+burger.addEventListener("click", function () {
     document.querySelector(".container").classList.toggle("show-menu");
 })
+
+document.querySelector("#qtde").addEventListener("change", atualizarpreco)
+document.querySelector("#js").addEventListener("change", atualizarpreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarpreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarpreco)
+document.querySelector("#prazo").addEventListener("change", function(){
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarpreco()
+})
+
+function atualizarpreco() {
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+    let preco = qtde * 100
+
+    if(temJS) preco *= 1.1
+
+    if(incluiLayout) preco += 500
+
+    let taxaUrgencia = 1 - prazo * 0.1;
+    preco *= 1 + taxaUrgencia
+
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
+
